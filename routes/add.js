dfe245182172ab0bff11a1211c2fbfbb8a1deede
mimi010912@g.skku.edu <mimi.hwemin.kim@gmail.com>
2023-04-12 05:11:07 +0900

    started index, add page

--- routes/add.js
+++ routes/add.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('view', { title: 'Express' });
-  res.render('index')
+    res.render('add')
 });
 
 module.exports = router;
